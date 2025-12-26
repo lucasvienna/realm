4f7a2d32af1fc4c44895c55c367f05e9a0bd8d52
Lucas Vienna <dev@lucasvienna.dev>
2025-12-26 21:32:57 +0000

    build: enable SvelteKit remote functions

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,17 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		experimental: {
+			remoteFunctions: true,
+		},
+	},
 	compilerOptions: {
 		runes: true,
+		experimental: {
+			async: true,
+		},
 	},
 };
 
